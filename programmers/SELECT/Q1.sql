bad15de85d12c0f600ef60b0b48f83149c16fcd0
junes7 <2junes@naver.com>
2021-10-26 07:53:53 +0900

    solve sql Q1 by using select

---
+++ programmers/SELECT/Q1.sql
@@ -0,0 +1,8 @@
+﻿-- method 1
+select *
+from animal_ins
+order by animal_id;
+-- method 2
+select animal_id, animal_type, datetime, intake_condition, name, sex_upon_intake
+from animal_ins
+order by animal_id;
