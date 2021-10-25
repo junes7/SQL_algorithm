2343fcc7c3bd0e30d2d6e651e9ea8b71f7edbfc8
junes7 <2junes@naver.com>
2021-10-26 08:04:24 +0900

    solve sql Q1 with procedure by using select

---
+++ programmers/SELECT/Q1_prd.sql
@@ -0,0 +1,13 @@
+﻿-- check previous procedure
+drop procedure if exists search;
+-- create procedure
+delimiter $$
+create procedure search()
+begin
+    select animal_id, animal_type, datetime, intake_condition, name, sex_upon_intake
+    from animal_ins
+    order by 1;
+end $$
+delimiter ;
+-- check result
+call search();
