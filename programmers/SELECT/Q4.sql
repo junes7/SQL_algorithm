de24a339cfabde606c9c4b018427d15b22e5c769
junes7 <2junes@naver.com>
2021-10-27 08:07:15 +0900

    solve sql Q4 by using select

---
+++ programmers/SELECT/Q4.sql
@@ -0,0 +1,10 @@
+﻿-- method 1
+select animal_id, name
+from animal_ins
+where intake_condition <> 'Aged'
+order by 1;
+-- method 2
+select animal_id, name
+from animal_ins
+where intake_condition not in('Aged')
+order by 1;
