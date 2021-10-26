6489530adc20674c4c2ddd266894b0b5e2ff94ae
junes7 <2junes@naver.com>
2021-10-27 08:19:23 +0900

    solve sql Q4 with procedure by using select

---
+++ programmers/SELECT/Q4_prd.sql
@@ -0,0 +1,14 @@
+﻿drop procedure if exists search2;
+
+delimiter $$
+create procedure search2(
+in cond varchar(7))
+begin
+	select animal_id, name
+    from animal_ins
+    where intake_condition <> cond
+    order by 1;
+end $$
+delimiter ;
+
+call search2('Aged');
