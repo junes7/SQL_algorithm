14aa4f1fd9dea66d26767fb820883317f2e4c134
junes7 <2junes@naver.com>
2021-10-27 07:35:27 +0900

    solve sql Q2 with procedure by using select

---
+++ programmers/SELECT/Q2_prd.sql
@@ -0,0 +1,12 @@
+﻿drop procedure if exists reverseSearch;
+
+delimiter $$
+create procedure reverseSearch()
+begin
+	select name, datetime
+    from animal_ins
+    order by animal_id desc;
+end $$
+delimiter ;
+
+call reverseSearch();
