72d4ae975ebba4978cdcffb885bb7af93884fa7b
junes7 <2junes@naver.com>
2021-10-27 07:58:31 +0900

    solve sql Q3 with procedure by using select

---
+++ programmers/SELECT/Q3_prd.sql
@@ -0,0 +1,14 @@
+﻿drop procedure if exists search1;
+
+delimiter $$
+create procedure search1(
+in cond varchar(7))
+begin
+	select animal_id, name
+    from animal_ins
+    where intake_condition=cond
+    order by 1;
+end $$
+delimiter ;
+
+call search1('Sick');
