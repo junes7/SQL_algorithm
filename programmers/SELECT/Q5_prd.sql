2c35a5cb656bd0f7d3c589fa60ae73748e04a3e7
junes7 <2junes@naver.com>
2021-10-28 07:30:15 +0900

    solve sql Q5 with procedure by using select

---
+++ programmers/SELECT/Q5_prd.sql
@@ -0,0 +1,12 @@
+﻿drop procedure if exists search3;
+
+delimiter $$
+create procedure search3()
+begin
+	select animal_id, name
+    from animal_ins
+    order by 1;
+end $$
+delimiter ;
+
+call search3();
