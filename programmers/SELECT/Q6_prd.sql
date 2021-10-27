7d3fabebc5e9f4631f725664bfb5e50cfc64b118
junes7 <2junes@naver.com>
2021-10-28 08:45:13 +0900

    solve sql Q6 with procedure by using select

---
+++ programmers/SELECT/Q6_prd.sql
@@ -0,0 +1,12 @@
+﻿drop procedure if exists search4;
+
+delimiter $$
+create procedure search4()
+begin
+	select animal_id, name, datetime
+    from animal_ins
+	order by 2, 3 desc;
+end $$
+delimiter ;
+
+call search4();
