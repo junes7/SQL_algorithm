fdd90fc869263a057496b906783de9a1f65985aa
junes7 <2junes@naver.com>
2021-10-28 09:14:00 +0900

    solve sql Q7 by using select

---
+++ programmers/SELECT/Q7.sql
@@ -0,0 +1,10 @@
+﻿-- method 1
+select name
+from animal_ins
+where datetime=(select min(datetime)
+				from animal_ins);
+-- method 2
+select name
+from animal_ins
+where datetime in(select min(datetime)
+				  from animal_ins);
