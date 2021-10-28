cd1b67d967df2eac96b958277e9b0136cdbe1c84
junes7 <2junes@naver.com>
2021-10-28 10:39:20 +0900

    solve sql Q7 with procedure by using select

---
+++ programmers/SELECT/Q7_prd.sql
@@ -0,0 +1,13 @@
+﻿drop procedure if exists search5;
+
+delimiter $$
+create procedure search5()
+begin
+	select name
+    from animal_ins
+    where datetime in(select min(datetime)
+					  from animal_ins);
+end $$
+delimiter ;
+
+call search5();
