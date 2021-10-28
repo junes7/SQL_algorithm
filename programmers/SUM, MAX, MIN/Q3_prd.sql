ed9de1fb13a565d5762e3c294fcdc8c30e92b1fd
junes7 <2junes@naver.com>
2021-10-29 07:45:52 +0900

    solve sql Q3 with procedure by using count

---
+++ programmers/SUM, MAX, MIN/Q3_prd.sql
@@ -0,0 +1,11 @@
+﻿drop procedure if exists animalNum;
+
+delimiter //
+create procedure animalNum()
+begin
+	select count(animal_id) as total
+    from animal_ins;
+end //
+delimiter ;
+
+call animalNum();
