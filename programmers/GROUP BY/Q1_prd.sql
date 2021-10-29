1990c44112c6c2b394bc89ae75bd0dc17af10c13
junes7 <2junes@naver.com>
2021-10-30 08:42:56 +0900

    solve sql Q1 with procedure by using group by

---
+++ programmers/GROUP BY/Q1_prd.sql
@@ -0,0 +1,13 @@
+﻿drop procedure if exists animalCount;
+
+delimiter //
+create procedure animalCount()
+begin
+	select animal_type, count(*) count
+    from animal_ins
+    group by 1
+    order by 1;
+end //
+delimiter ;
+
+call animalCount();
