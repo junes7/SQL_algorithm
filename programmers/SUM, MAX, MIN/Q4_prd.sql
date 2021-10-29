bf0428aeced0ba85d338e83d8ec6ad8bc3057b4a
junes7 <2junes@naver.com>
2021-10-30 07:27:04 +0900

    solve sql Q4 with procedure by using count

---
+++ programmers/SUM, MAX, MIN/Q4_prd.sql
@@ -0,0 +1,12 @@
+﻿drop procedure if exists nameNum;
+
+delimiter //
+create procedure nameNum()
+begin
+	select count(distinct name) as count
+    from animal_ins
+	where name is not null;
+end //
+delimiter ;
+
+call nameNum();
