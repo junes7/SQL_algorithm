b9a20eec5424ffd61d857834f090f0bbaf4516c1
junes7 <2junes@naver.com>
2021-10-30 09:11:27 +0900

    solve sql Q2 with procedure by using group by

---
+++ programmers/GROUP BY/Q2_prd.sql
@@ -0,0 +1,15 @@
+﻿drop procedure if exists nameCount;
+
+delimiter //
+create procedure nameCount()
+begin
+	select name, count(name) count
+    from animal_ins
+	where name is not null
+	group by 1
+    having count(name) >= 2
+    order by 1;
+end //
+delimiter ;
+
+call nameCount();
