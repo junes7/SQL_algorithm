b6c83d5a650494b0571d3377a387998edc061fd7
junes7 <2junes@naver.com>
2021-11-01 08:02:23 +0900

    solve sql Q2 with procedure by using IS NULL

---
+++ programmers/IS NULL/Q2_prd.sql
@@ -0,0 +1,13 @@
+﻿drop procedure if exists isnotnull;
+
+delimiter //
+create procedure isnotnull()
+begin
+	select animal_id
+    from animal_ins
+    where name is not null
+    order by 1;
+end //
+delimiter ;
+
+call isnotnull();
