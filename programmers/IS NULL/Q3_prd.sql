2f8b84789b9b35c740153d87a253f78417602dc2
junes7 <2junes@naver.com>
2021-11-01 08:05:47 +0900

    solve sql Q3 with procedure by using IFNULL

---
+++ programmers/IS NULL/Q3_prd.sql
@@ -0,0 +1,12 @@
+﻿drop procedure ifnull;
+
+delimiter //
+create procedure ifnull()
+begin
+	select animal_type, ifnull(name, "No name") name, sex_upon_intake
+    from animal_ins
+    order by animal_id;
+end //
+delimiter ;
+
+call ifnull();
