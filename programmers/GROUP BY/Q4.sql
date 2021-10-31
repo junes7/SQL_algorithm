ff13652dd0a1bc4a4547c02f57961c3536656452
junes7 <2junes@naver.com>
2021-10-31 10:09:12 +0900

    solve sql Q4 by using group by and hour function

---
+++ programmers/GROUP BY/Q4.sql
@@ -0,0 +1,8 @@
+﻿set @HourList:=-1;
+select (@HourList := @HourList + 1) as HOUR,
+(select count(*) from animal_outs
+ where hour(datetime)=@HourList) as COUNT
+from animal_outs
+where @HourList < 23
+group by 1
+order by 1;
