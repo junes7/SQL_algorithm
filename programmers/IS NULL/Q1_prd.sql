2d4990d38e8409816260b80ea28a23bdafb5917f
junes7 <2junes@naver.com>
2021-11-01 06:43:05 +0900

    solve sql Q1 with procedure by using IS NULL

---
+++ programmers/IS NULL/Q1_prd.sql
@@ -0,0 +1,13 @@
+﻿drop procedure if exists isnull1;
+
+delimiter //
+create procedure isnull1()
+begin
+	select animal_id
+    from animal_ins
+	where name is null
+	order by 1;
+end //
+delimiter ;
+
+call isnull1();
