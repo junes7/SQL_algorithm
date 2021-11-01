5ca4a3c7d0e351ca19fa10c79177d5083af839ae
junes7 <2junes@naver.com>
2021-11-02 07:42:18 +0900

    solve sql Q1 with procedure by using JOIN

---
+++ programmers/JOIN/Q1_prd.sql
@@ -0,0 +1,13 @@
+﻿drop procedure if exists join1;
+
+delimiter //
+create procedure join1()
+begin
+	select animal_id, name
+	from animal_outs
+	where animal_id not in(select animal_id
+						   from animal_ins);
+end //
+delimiter ;
+
+call join1();
