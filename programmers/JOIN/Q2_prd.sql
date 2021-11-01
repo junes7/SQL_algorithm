d2bfe0d13066e0700c26a68fa10545665eaf6aef
junes7 <2junes@naver.com>
2021-11-02 08:20:28 +0900

    solve sql Q2 with procedure by using inner JOIN

---
+++ programmers/JOIN/Q2_prd.sql
@@ -0,0 +1,15 @@
+﻿drop procedure if exists join2;
+
+delimiter //
+create procedure join2()
+begin
+	select ai.animal_id, ao.name
+    from animal_ins ai
+		inner join animal_outs ao
+		on ai.animal_id=ao.animal_id
+	where ai.datetime > ao.datetime
+	order by ai.datetime;
+end //
+delimiter ;
+
+call join2();
