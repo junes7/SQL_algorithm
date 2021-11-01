17889bcc1885c877a7d55c418d7a18285d9b4c8d
junes7 <2junes@naver.com>
2021-11-02 08:19:36 +0900

    modify some sql files and solve sql Q2 by using JOIN

--- programmers/JOIN/Q1.sql
+++ programmers/JOIN/Q1.sql
@@ -1,4 +1,5 @@
 ﻿select animal_id, name
 from animal_outs
 where animal_id not in(select animal_id
-                       from animal_ins);
+                       from animal_ins)
+order by 1;
--- programmers/JOIN/Q1_prd.sql
+++ programmers/JOIN/Q1_prd.sql
@@ -7,6 +7,7 @@ begin
 	from animal_outs
 	where animal_id not in(select animal_id
 						   from animal_ins);
+	order by 1;
 end //
 delimiter ;
 
