2dea71baba056f474b0f2992e546763200474929
junes7 <2junes@naver.com>
2021-11-03 06:53:24 +0900

    solve sql Q4 with procedure by inner JOIN

---
+++ programmers/JOIN/Q4_prd.sql
@@ -0,0 +1,15 @@
+﻿drop procedure if exists join4;
+
+delimiter //
+create procedure join4()
+begin
+	select ai.animal_id, ai.animal_type, ai.name
+    from animal_ins ai
+		inner join animal_outs ao
+        on ai.animal_id=ao.animal_id
+	where ai.sex_upon_intake <> ao.sex_upon_outcome
+    order by 1;
+end //
+delimiter ;
+
+call join4();
