09ff65fed68b3be0f9f69a49363b784aee443aea
junes7 <2junes@naver.com>
2021-11-03 06:48:12 +0900

    solve sql Q4 by using inner JOIN

---
+++ programmers/JOIN/Q4.sql
@@ -0,0 +1,6 @@
+﻿select ai.animal_id, ai.animal_type, ai.name
+from animal_ins ai
+    inner join animal_outs ao
+    on ai.animal_id=ao.animal_id
+where ai.sex_upon_intake <> ao.sex_upon_outcome
+order by 1;
