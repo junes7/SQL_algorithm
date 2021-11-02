a7b94fef573e7d583eaa67a5eb0e1f897628d796
junes7 <2junes@naver.com>
2021-11-02 09:37:51 +0900

    solve sql Q3 with procedure by using correlated subqurey

---
+++ programmers/JOIN/Q3_prd.sql
@@ -0,0 +1,15 @@
+﻿drop procedure if exists join3;
+
+delimiter //
+create procedure join3()
+begin
+	select name, datetime
+    from animal_ins
+    where animal_id not in (select animal_id
+							from animal_outs)
+	order by 2
+    limit 3;
+end //
+delimiter ;
+
+call join3();
