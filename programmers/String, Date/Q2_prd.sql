72876d83d1f3327a9a4ddc7d9d2adb2408a23df9
junes7 <2junes@naver.com>
2021-11-03 09:42:12 +0900

    solve sql Q2 with procedure by using where condtion and order by

---
+++ programmers/String, Date/Q2_prd.sql
@@ -0,0 +1,18 @@
+﻿drop procedure if exists findAnimal;
+
+desc animal_ins;
+delimiter //
+create procedure findAnimal(
+in typeA varchar(3),
+in input varchar(12))
+begin
+	select animal_id, name
+    from animal_ins
+    where animal_type=typeA and
+    name like input
+    order by 2;
+end //
+delimiter ;
+
+call findAnimal('Dog', '%el%');
+call findAnimal('Cat', '%el%');
