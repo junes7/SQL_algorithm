60a48773eaf141df9d36157e98bbe3d44ac74b01
junes7 <2junes@naver.com>
2021-11-04 07:24:34 +0900

    solve sql Q3 with procedure by using case statement and order by

---
+++ programmers/String, Date/Q3_prd.sql
@@ -0,0 +1,14 @@
+﻿drop procedure if exists findNeut;
+
+delimiter //
+create procedure findNeut(
+in input varchar(13))
+begin
+	select animal_id, name,
+	case when sex_upon_intake like input then 'X' else 'O' end 중성화
+    from animal_ins
+    order by 1;
+end //
+delimiter ;
+
+call findNeut('intact%');
