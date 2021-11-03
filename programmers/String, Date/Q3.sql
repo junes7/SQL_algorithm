b79c66e51a7fb1ba73368ec535e88e7bebbde3fe
junes7 <2junes@naver.com>
2021-11-04 07:23:42 +0900

    solve sql Q3 by using case statement and order by

---
+++ programmers/String, Date/Q3.sql
@@ -0,0 +1,10 @@
+﻿-- method 1
+select animal_id, name,
+case when sex_upon_intake like '%Neutered%' or sex_upon_intake like '%Spayed%' then 'O' else 'X' end 중성화
+from animal_ins
+order by 1;
+-- method 2
+select animal_id, name,
+case when sex_upon_intake like 'intact%' then 'X' else 'O' end 중성화
+from animal_ins
+order by 1;
