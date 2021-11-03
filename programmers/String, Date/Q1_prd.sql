218523a9f202ea967525ef008e6e86f05b4a133c
junes7 <2junes@naver.com>
2021-11-03 09:18:14 +0900

    solve sql Q1 with procedure by in method

---
+++ programmers/String, Date/Q1_prd.sql
@@ -0,0 +1,13 @@
+﻿drop procedure if exists nameList;
+
+delimiter //
+create procedure nameList()
+begin
+	select animal_id, name, sex_upon_intake
+    from animal_ins
+    where name in('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+    order by 1;
+end //
+delimiter ;
+
+call nameList();
