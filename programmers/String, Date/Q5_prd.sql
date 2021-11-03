1cf9b45e9ead02c6de55d26b3f92fba54707c175
junes7 <2junes@naver.com>
2021-11-04 08:09:18 +0900

    solve sql Q5 with procedure by using date_format method and order by

---
+++ programmers/String, Date/Q5_prd.sql
@@ -0,0 +1,12 @@
+﻿drop procedure if exists dateList;
+
+delimiter //
+create procedure dateList()
+begin
+	select animal_id, name, date_format(datetime, '%Y-%m-%d') 날짜
+    from animal_ins
+    order by 1;
+end //
+delimiter ;
+
+call dateList();
